2f8725080280e138165f1ba09da3ac21a9a11a73
zzchen_acer <zzchen@g.ucla.edu>
2022-01-27 22:50:39 -0800

    ch05 Adding a Sphere

--- RayTracingGPU/Main.cu
+++ RayTracingGPU/Main.cu
@@ -18,16 +18,28 @@ void check_cuda(int result, char const* const func, const char* const file, int 
 }
 
 // Render
+__device__ bool hit_sphere(const point3& center, double radius, const ray& r)
+{
+	vec3 oc = r.origin() - center; 
+	double a = dot(r.direction(), r.direction());
+	double b = 2.0 * dot(oc, r.direction()); 
+	double c = dot(oc, oc) - radius * radius;
+	double discriminant = b * b - 4.0 * a * c; 
+	return discriminant > 0.0; 
+}
+
 __device__ color3 ray_color(const ray& r)
 {
+	if (hit_sphere(vec3(0, 0, -1), 0.5, r))
+		return color3(1, 0, 0);
 	vec3 unit_direction = unit_vector(r.direction());
 	double t = 0.5 * (unit_direction.y() + 1.0f); 
 	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
 __global__ void render(vec3* fb, int max_x, int max_y,
-					   vec3 lower_left_corner, vec3 horizontal, vec3 vertical,
-					   vec3 origin )
+					   vec3 lower_left_corner, vec3 horizontal, 
+					   vec3 vertical, vec3 origin)
 {
 	int i = threadIdx.x + blockIdx.x * blockDim.x;
 	int j = threadIdx.y + blockIdx.y * blockDim.y;
